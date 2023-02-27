16e92d3e7b99fb57830083b340e385dcf3c572d0
Eyasmin Basher <eyasminbasher@gmail.com>
2023-02-27 21:45:49 +0000

    added game functions file

--- game3.py
+++ game3.py
@@ -1,71 +1,20 @@
-import random  # imported the random module to use the randint method
-# import game_functions
+import game_functions
 
-# this version is the same as game2.py but has a score too
+# this version is the same as game2.py but has a score and the functions are in another file
 
-# assigning rock, paper, scissors to game variable in a list
-game = ['R', 'P', 'S']
-computer = str(game[random.randint(0, 2)])
-
-
-# Boolean value, it's not assigning True to a variable
 player = True
 
 player_score = 0
 comp_score = 0
-# NEW ******************** start
-# if player == 'R':
-#     player == 'Rock'
-# elif player == 'P':
-#     player == 'Paper'
-# elif player == 'S':
-#     player == 'Scissors'
-# else:
-#     print(f"That's not a valid input. Please input again.")
-# NEW ******************** end
-
-
-# Keeps asking the player for input until they enter "R", "P", or "S" only. Invalid if they don't.
-def player_choice():
-    player = input("Rock, Paper, Scissors? Input R, P or S: ").upper()
-    while player != 'R' and player != 'P' and player != 'S':  # this should be or, but it doesn't work, so I put and
-        print(f"That's not a valid input. Please input again.")
-        player = input("Rock, Paper, Scissors? Input R, P or S: ").upper()
-    return player
-    # if player == 'R':
-    #     player == 'Rock'
-    # elif player == 'P':
-    #     player == 'Paper'
-    # elif player == 'S':
-    #     player == 'Scissors'
-
-
-# Randomly selects either "rock", "paper" or "scissors" for computer to return from game list
-def comp_choice():
-    computer = game[random.randint(0, 2)]
-    return computer
-
-
-# This returns true if there is a tie
-def game_tie(player, computer):
-    return player == computer
-
-
-# This returns true if the player wins
-def determine_winner(player, computer):
-    winner = (player == "R" and computer == "S") or (player == "P" and computer == "R") or (
-            player == "S" and computer == "P")
-    return winner
-
 
 # used a while loop to keep it running while the condition is True, listing all the possible outcomes.
 while player:
     # sets the score for both to 0
 
     # this calls the player_choice function
-    player = player_choice()
+    player = game_functions.player_choice()
     # this calls the comp_choice function
-    computer = comp_choice()
+    computer = game_functions.comp_choice()
 
     # prints the choices
     if player == 'R':
@@ -82,24 +31,12 @@ while player:
     if computer == 'S':
         print('Computer played Scissors.')
 
-    # NEW ******************** start
-    # if player == 'R':
-    #     player == 'Rock'
-    # elif player == 'P':
-    #     player == 'Paper'
-    # elif player == 'S':
-    #     player == 'Scissors'
-    # else:
-    #     print(f"That's not a valid input. Please input again.")
-    # NEW ******************** end
-
-    # this calls the tie function and checks it with an if statement
-    if game_tie(player, computer):
+    if game_functions.game_tie(player, computer):
         player_score += 1
         comp_score += 1
         print(f"It's a tie! Computer: {comp_score}, You: {player_score}")
 
-    elif determine_winner(player, computer):
+    elif game_functions.determine_winner(player, computer):
         player_score += 1
         print(f"You win! Computer: {comp_score}, You: {player_score}")
     else:
---
+++ game_functions.py
@@ -0,0 +1,30 @@
+# game functions
+import random
+
+game = ['R', 'P', 'S']
+
+
+def player_choice():
+    player = input("Rock, Paper, Scissors? Input R, P or S: ").upper()
+    while player != 'R' and player != 'P' and player != 'S':  # this should be or, but it doesn't work, so I put and
+        print(f"That's not a valid input. Please input again.")
+        player = input("Rock, Paper, Scissors? Input R, P or S: ").upper()
+    return player
+
+
+# Randomly selects either "rock", "paper" or "scissors" for computer to return from game list
+def comp_choice():
+    computer = game[random.randint(0, 2)]
+    return computer
+
+
+# This returns true if there is a tie
+def game_tie(player, computer):
+    return player == computer
+
+
+# This returns true if the player wins
+def determine_winner(player, computer):
+    winner = (player == "R" and computer == "S") or (player == "P" and computer == "R") or (
+            player == "S" and computer == "P")
+    return winner
